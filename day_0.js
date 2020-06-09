0c8a6a8cb188af498de727828704709a5af789d4
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-06-09 13:51:09 -0400

    day 0

--- day_0.js
+++ day_0.js
@@ -1,5 +1,16 @@
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
 function greeting(parameterVariable) {
-  console.log('Hello, World!')
-  console.log('Welcome to 10 Days of JavaScript!')
-}
+  // This line prints 'Hello, World!' to the console:
+  console.log('Hello, World!');
+  
 
+  // Write a line of code that prints parameterVariable to stdout using console.log:
+console.log(parameterVariable);
+
+}
