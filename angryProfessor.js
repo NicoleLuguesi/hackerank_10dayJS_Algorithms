5fe973325336d3f24539467add9cee1948b32af5
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-07-17 10:59:11 -0400

    angry professor algorithm

---
+++ angryProfessor.js
@@ -0,0 +1,9 @@
+function angryProfessor(minStudents, arrivalTimes) {
+  arrivalTimes = arrivalTimes.filter((time) => time <= 0);
+  if (arrivalTimes.length >= minStudents) {
+    return 'NO';
+  } else {
+    return 'YES';
+  }
+}
+console.log(angryProfessor(3, [-1, 3, 2, 0, 0]));
