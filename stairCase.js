c681563ffd199089ad07da2f88c0d45537c79347
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-07-17 10:47:05 -0400

    staircase algorithm

---
+++ stairCase.js
@@ -0,0 +1,21 @@
+// Complete the staircase function below.
+function staircase(n) {
+
+  for ( let i = 1; i <= n; i++ ) {
+    console.log(" ".repeat(n - i)+ "#".repeat(i))   
+  }  
+}
+staircase(6)
+
+// A single integer, n , denoting the size of the staircase.
+
+// Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+// Write a program that prints a staircase of size n.
+
+// Print a staircase of size  using # symbols and spaces.
+
+
+
+
+  
